12025920ba410be6483cd0992d7ae0253e5e70b9
kimsin3003@naver.com <xodn**26>
2015-09-06 23:40:13 +0900

    facebook - 2nd Task Completed

--- facebook/facebook.js
+++ facebook/facebook.js
@@ -1,8 +1,18 @@
 var newsfeed = document.getElementById("newsfeed");
 newsfeed.addEventListener('click', function(event){
     var likeButton = event.target.closest('.likeButton');
-    likeButton.style.backgroundImage = "url('./asset/like.png')";
-    console.log(event.target.closest('button').backgroundImage);
+    if(!likeButton.firstChild.checked){
+        likeButton.style.backgroundImage = "url('./asset/like.png')";
+        likeButton.getElementsByTagName("input")[0].checked = true;
+        console.log(likeButton.parentNode);
+        likeButton.parentNode.getElementsByClassName("likeCount")[0].innerHTML = "1";
+    }
+    else{
+        likeButton.style.backgroundImage = "url('./asset/unlike.png')";
+        likeButton.getElementsByTagName("input")[0].checked = false;
+        likeButton.parentNode.getElementsByClassName("likeCount")[0].innerHTML = "0";
+    }
+        
 });
 for(i = 0; i < 3; i++){
 	var newFeed = document.createElement('div');
@@ -14,6 +24,10 @@ for(i = 0; i < 3; i++){
     var buttonsDiv = document.createElement('div');
     var likeButton = document.createElement('button');
     likeButton.className = "likeButton";
+    var likeToggle = document.createElement('input');
+    likeToggle.type = 'raio';
+    likeToggle.className = 'likeToggle';
+    likeButton.appendChild(likeToggle);
     var commentButton = document.createElement('button');
     commentButton.className = "commentButton";
     var shareButton = document.createElement('button');
@@ -22,20 +36,62 @@ for(i = 0; i < 3; i++){
     newFeed.appendChild(commentButton);
     newFeed.appendChild(shareButton);
 	newsfeed.appendChild(newFeed);
+    var newHr = document.createElement('hr');
+    newHr.style.marginTop = "-3px";
+    newHr.style.opacity = "0.5";
+    newFeed.appendChild(newHr);
+    var num = document.createElement('span');
+    num.style.fontSize = "12px";
+    num.innerHTML = "0";
+    num.className = "likeCount";
+    var text = document.createElement('span');
+    text.style.fontSize = "12px";
+    text.innerHTML = "명이 좋아합니다";
+    newFeed.appendChild(num);
+    newFeed.appendChild(text);
     
 }
 
 window.addEventListener("scroll", function(){
-	var body = document.body;
-	var scrollToBottom = body.scrollHeight - body.scrollTop;
-	
+	var scrollToBottom = document.body.scrollHeight - document.body.scrollTop;
 	if(scrollToBottom < window.innerHeight){
-		var newDiv = document.createElement('div');
-		var newHr = document.createElement('hr');
-		newDiv.classList.add("feed");
-		newDiv.appendChild(newHr);
-		newsfeed.appendChild(newDiv);
+        console.log("adfs");
+		var newFeed = document.createElement('div');
+        var random = Math.floor(Math.random()*10) + 1;
+        var iner = `<div><img width = "456px" height = "330px" src = \"./asset/${random}.jpg\"></div>
+        <hr width = "455px" style = "opacity : 0.5"></hr>`;
 
+        newFeed.innerHTML = iner;
+        newFeed.classList.add('feed');
+        var buttonsDiv = document.createElement('div');
+        var likeButton = document.createElement('button');
+        likeButton.className = "likeButton";
+        var likeToggle = document.createElement('input');
+        likeToggle.type = 'raio';
+        likeToggle.className = 'likeToggle';
+        likeButton.appendChild(likeToggle);
+        var commentButton = document.createElement('button');
+        commentButton.className = "commentButton";
+        var shareButton = document.createElement('button');
+        shareButton.className = "shareButton";
+        newFeed.appendChild(likeButton);
+        newFeed.appendChild(commentButton);
+        newFeed.appendChild(shareButton);
+        newsfeed.appendChild(newFeed);
+        var newHr = document.createElement('hr');
+        newHr.style.marginTop = "-3px";
+        newHr.style.opacity = "0.5";
+        newFeed.appendChild(newHr);
+        var num = document.createElement('span');
+        num.style.fontSize = "12px";
+        num.innerHTML = "0";
+        num.className = "likeCount";
+        var text = document.createElement('span');
+        text.style.fontSize = "12px";
+        text.innerHTML = "명이 좋아합니다";
+        newFeed.appendChild(num);
+        newFeed.appendChild(text);
+    
 
 	}
 
